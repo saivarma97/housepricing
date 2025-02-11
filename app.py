a8b420c11e5b5ca8c6d45b53ee1286597f6b5bd1
Shivasai <saivarmajs007@gmail.com>
2025-02-12 00:11:29 +0530

    adding new files

---
+++ app.py
@@ -0,0 +1,26 @@
+import pickle
+from flask import Flask, request, app, jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__) #starting point the application
+regmodel = pickle.load(open('regmodel.pkl', 'rb'))
+scalar = pickle.load(open('scalar.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api', methods = ['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    values = np.array(list(data.values())).reshape(1, -1)
+    print(values)
+    new_data = scalar.transform(values)
+    output = regmodel.predit(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
